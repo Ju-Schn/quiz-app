8e104d3fce4f93a24e46756f0d3bcd8b34153e4d
Julia Schneider <julia_schneider_mail@posteo.de>
2022-02-16 15:34:21 +0100

    Change design buttons

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const bookmarkButton = document.querySelector('[data-js=bookmarkButton]');
+const bookmarkButton = document.querySelectorAll('[data-js=bookmarkButton]');
+const card = document.querySelector('[data-js=card]');
+const answerButton = document.querySelector('[data-js=answerButton]');
 
 bookmarkButton.addEventListener('click', () => {
   bookmarkButton.classList.toggle('button__bookmark--bookmarked');
+  card.classList.toggle('card--bookmarked');
+  answerButton.classList.toggle('button__answer--bookmarked');
 });
