8f4464e430f236e5107eb79fa926b9669878de86
Julia Schneider <julia_schneider_mail@posteo.de>
2022-02-17 10:21:13 +0100

    Update JS for Show Answer Button

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const bookmarkButton = document.querySelector('[data-js=bookmarkButton]');
-const card = document.querySelector('[data-js=card]');
 const answerButton = document.querySelector('[data-js=answerButton]');
+const card = document.querySelector('[data-js=card]');
+const cardAnswerTitle = document.querySelector('[data-js=cardAnswerTitle]');
 
 bookmarkButton.addEventListener('click', () => {
   bookmarkButton.classList.toggle('button__bookmark--bookmarked');
   card.classList.toggle('card--bookmarked');
   answerButton.classList.toggle('button__answer--bookmarked');
+});
+
+answerButton.addEventListener('click', () => {
+  cardAnswerTitle.classList.toggle('card__show-answer');
 });
