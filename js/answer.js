8affff1d5c9b2a3024fd96f32eba34855a77f55f
Julia Schneider <julia_schneider_mail@posteo.de>
2022-02-19 17:10:19 +0100

    Add Hide Answer to answerButton

--- js/answer.js
+++ js/answer.js
@@ -5,10 +5,14 @@ export default function answer() {
     const cardAnswerTitle = card.querySelector('[data-js=cardAnswerTitle]');
     const cardAnswer = card.querySelector('[data-js=cardAnswer');
     const answerButton = card.querySelector('[data-js=answerButton]');
+    const showButton = card.querySelector('[data-js=showButton]');
+    const hideButton = card.querySelector('[data-js=hideButton]');
 
     answerButton.addEventListener('click', () => {
       cardAnswerTitle.classList.toggle('card__answer');
       cardAnswer.classList.toggle('card__answer');
+      showButton.classList.toggle('hidden');
+      hideButton.classList.toggle('hidden');
     });
   });
 
