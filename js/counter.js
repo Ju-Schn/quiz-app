2b5915b4d11a08d351abc7aed619879191c6235f
Julia Schneider <julia_schneider_mail@posteo.de>
2022-02-21 07:23:06 +0100

    add function to answer counter

--- js/counter.js
+++ js/counter.js
@@ -3,12 +3,18 @@ export default function counter() {
   const counterAddQuestion = document.querySelector(
     '[data-js=counterAddQuestion]'
   );
-  /*const createAnswer = document.querySelector('[data-js=createAnswer]');
-  const counterAddAnswer = document.querySelector('[data-js=counterAddAnswer]');*/
+  const createAnswer = document.querySelector('[data-js=createAnswer]');
+  const counterAddAnswer = document.querySelector('[data-js=counterAddAnswer]');
 
   createQuestion.addEventListener('input', () => {
     const questionTextLength = createQuestion.value.length;
     const maxLengthQuestion = createQuestion.maxLength;
     counterAddQuestion.innerText = maxLengthQuestion - questionTextLength;
+  });
+
+  createAnswer.addEventListener('input', () => {
+    const answerTextLength = createAnswer.value.length;
+    const maxLengthAnswer = createQuestion.maxLength;
+    counterAddAnswer.innerText = maxLengthAnswer - answerTextLength;
   });
 }
